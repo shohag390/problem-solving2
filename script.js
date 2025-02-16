ee5dabb693aa13d591919a69f4db87906b3f09c1
shohag390 <shohag390@users.noreply.github.com>
2025-02-16 22:25:00 +0600

    first Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function compaire(num1, num2, num3) {
+    if (num1 > num2 && num1 > num3) {
+        console.log("num1 is biggest");
+    } else if (num2 > num1 && num2 > num3) {
+        console.log("num2 is biggest ");
+    } else {
+        console.log("num3 is biggest");
+    }
+}
+
+compaire(2000, 90, 400)
+
