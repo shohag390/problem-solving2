4d16ade933042572d40c799c48bbe797a1f46764
shohag390 <shohag390@users.noreply.github.com>
2025-02-16 22:28:23 +0600

    Age compaire

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-function compaire(num1, num2, num3) {
-    if (num1 > num2 && num1 > num3) {
-        console.log("num1 is biggest");
-    } else if (num2 > num1 && num2 > num3) {
-        console.log("num2 is biggest ");
-    } else {
-        console.log("num3 is biggest");
-    }
-}
+// function compaire(num1, num2, num3) {
+//     if (num1 > num2 && num1 > num3) {
+//         console.log("num1 is biggest");
+//     } else if (num2 > num1 && num2 > num3) {
+//         console.log("num2 is biggest ");
+//     } else {
+//         console.log("num3 is biggest");
+//     }
+// }
 
-compaire(2000, 90, 400)
+// compaire(2000, 90, 400)
 
+// const summa = 10;
+// const mowsumi = 2;
+
+// if (summa > mowsumi) {
+//     console.log("summa is older ");
+
+// } else {
+//     console.log("mowsumi is older");
+
+// }
